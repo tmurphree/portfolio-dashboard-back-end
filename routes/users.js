143d5c27a8110af07680c6618046a8a962e6064d
Tom Murphree <tmurph1810@gmail.com>
2019-03-07 21:35:11 -0600

    update users route message

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('The users route is under construction.  Please check back later.');
 });
 
 module.exports = router;
