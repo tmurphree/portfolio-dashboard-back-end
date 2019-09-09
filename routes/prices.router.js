d35f1fa724e3db034bc44b62581868327ec535c2
Tom Murphree <tmurph1810@gmail.com>
2019-09-09 14:05:27 -0500

    finish /prices/many

--- routes/prices.router.js
+++ routes/prices.router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getLatestPrice } = require('../controllers/prices.controller');
+const { getLatestPrice, getManyPrices } = require('../controllers/prices.controller');
 
 const router = express.Router();
 
@@ -11,7 +11,6 @@ router.post('/one', (req, res) => {
 
   getLatestPrice(req.body.symbol)
     .then((result) => {
-      console.log(result);
       res.status(200).json(result);
     })
     .catch((err) => {
@@ -20,8 +19,27 @@ router.post('/one', (req, res) => {
     });
 });
 
-router.post('/many', () => {
+router.post('/many', (req, res) => {
+  if (!req.body.symbol) {
+    res.status(400).json({ error: true, errorMessage: 'Bad request -- no symbol in body.' });
+    return;
+  }
 
+  if (!Array.isArray(req.body.symbol)) {
+    res
+      .status(400)
+      .json({ error: true, errorMessage: 'Bad request -- symbol in body must be an array.' });
+    return;
+  }
+
+  getManyPrices(req.body.symbol)
+    .then((result) => {
+      res.status(200).json({ payload: result });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: true, errorMessage: 'Server error.' });
+    });
 });
 
 module.exports = router;
--- spec/integrationTests/prices.spec.js
+++ spec/integrationTests/prices.spec.js
@@ -29,7 +29,7 @@ const myReporter = {
 jasmine.getEnv().addReporter(myReporter);
 // #endregion jasmine setup
 
-describe('prices /one', () => {
+describe('/prices/one', () => {
   const url = 'http://127.0.0.1:3000/prices/one';
 
   it('responds with a 400 error if no req.body.symbol', () => {
@@ -52,7 +52,35 @@ describe('prices /one', () => {
         expect(res.data.price).toBeGreaterThan(20);
       })
       .catch((res) => {
+        fail(`Expected to succeed but got ${res}`);
+      });
+  });
+});
+
+describe('/prices/many', () => {
+  const url = 'http://127.0.0.1:3000/prices/many';
+
+  it('responds with a 400 error if no req.body.symbol', () => {
+    axios.post(url, {})
+      .then((res) => {
         fail(`Expected to fail but got ${res}`);
+      })
+      .catch((res) => {
+        expect(res.response.status).toBe(400);
+        expect(res.data.error).toBe(true);
+        expect(res.data.errorMessage).toBeTruthy();
+      });
+  });
+
+  it('responds with a 200 response if a valid request is sent', () => {
+    axios.post(url, { symbol: ['bac', 'msft'] })
+      .then((res) => {
+        console.log(res.data);
+        expect(res.response.status).toBe(200);
+        expect(Array.isArray(response.data.payload)).toBe(true);
+      })
+      .catch((res) => {
+        fail(`Expected to succeed but got ${res}`);
       });
   });
 });
